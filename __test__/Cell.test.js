1692ce0ff6ed95ff834c8f1eb03d0befbea23052
Nando San Miguel <mythemc@gmail.com>
2021-11-22 19:08:11 -0600

    Fix first test
    Co-authored-by: danthek <abraham8507@gmail.com>
    Co-authored-by: Hugoneytor <halvarez0@ucol.mx>
    Co-authored-by: adrianagh13 <gomezhinojosa.adriana@gmail.com>

--- __test__/Cell.test.js
+++ __test__/Cell.test.js
@@ -3,8 +3,7 @@ const Cell = require("../src/models/Cell");
 describe("Generate a random value between O and .", () => {
     test("it should generate O or .", () => {
         const cell = new Cell();
-        const value1 = "O";
-        const value2 = ".";
-        expect(cell.createCells()).toEqual(value1 || value2);
+        const result = cell.createCells();
+        expect(result == "O" || result == ".").toBeTruthy();
     });
 });
