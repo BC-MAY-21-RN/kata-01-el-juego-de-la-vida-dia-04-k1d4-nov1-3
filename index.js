5f5cbf8edde6cf539d8625be1f0cef6cc392710a
Nando San Miguel <mythemc@gmail.com>
2021-11-23 17:06:12 -0600

    Fix readme
    Co-authored-by: xManuel007   <maanuu.009@gmail.com>
    Co-authored-by: adrianagh13  <gomezhinojosa.adriana@gmail.com>
    Co-authored-by: David-GuzmanD  <davidt.guzman10@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 "use strict";
-/*
-  Co-authored-by: danthek <abraham8507@gmail.com>
-  Co-authored-by: Hugoneytor <halvarez0@ucol.mx>
-  Co-authored-by: adrianagh13 <gomezhinojosa.adriana@gmail.com>
-  Co-authored-by: Namoku <sanmiguel.nando@gmail.com>
-*/
 
 const Grid = require("./src/models/Grid");
 
@@ -15,3 +9,24 @@ grid.printGrid();
 grid.printArray();
 grid.newGrid();
 grid.printGrid();
+
+//Ejemplo de poner co-autores
+// Co-authored-by: xManuel007   <maanuu.009@gmail.com>
+// Co-authored-by: adrianagh13  <gomezhinojosa.adriana@gmail.com>
+// Co-authored-by: David-GuzmanD  <davidt.guzman10@gmail.com>
+
+// ⠸⣷⣦⠤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⠀⠀⠀
+// ⠀⠙⣿⡄⠈⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠔⠊⠉⣿⡿⠁⠀⠀⠀
+// ⠀⠀⠈⠣⡀⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⠁⠀⠀⣰⠟⠀⠀⠀⣀⣀
+// ⠀⠀⠀⠀⠈⠢⣄⠀⡈⠒⠊⠉⠁⠀⠈⠉⠑⠚⠀⠀⣀⠔⢊⣠⠤⠒⠊⠉⠀⡜
+// ⠀⠀⠀⠀⠀⠀⠀⡽⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠩⡔⠊⠁⠀⠀⠀⠀⠀⠀⠇
+// ⠀⠀⠀⠀⠀⠀⠀⡇⢠⡤⢄⠀⠀⠀⠀⠀⡠⢤⣄⠀⡇⠀⠀⠀⠀⠀⠀⠀⢰⠀
+// ⠀⠀⠀⠀⠀⠀⢀⠇⠹⠿⠟⠀⠀⠤⠀⠀⠻⠿⠟⠀⣇⠀⠀⡀⠠⠄⠒⠊⠁⠀
+// ⠀⠀⠀⠀⠀⠀⢸⣿⣿⡆⠀⠰⠤⠖⠦⠴⠀⢀⣶⣿⣿⠀⠙⢄⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⢻⣿⠃⠀⠀⠀⠀⠀⠀⠀⠈⠿⡿⠛⢄⠀⠀⠱⣄⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⢸⠈⠓⠦⠀⣀⣀⣀⠀⡠⠴⠊⠹⡞⣁⠤⠒⠉⠀⠀
+// ⠀⠀⠀⠀⠀⠀⣠⠃⠀⠀⠀⠀⡌⠉⠉⡤⠀⠀⠀⠀⢻⠿⠆⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠰⠁⡀⠀⠀⠀⠀⢸⠀⢰⠃⠀⠀⠀⢠⠀⢣⠀⠀⠀⠀⠀
+// ⠀⠀⠀⢶⣗⠧⡀⢳⠀⠀⠀⠀⢸⣀⣸⠀⠀⠀⢀⡜⠀⣸⢤⣶⠀
+// ⠀⠀⠀⠈⠻⣿⣦⣈⣧⡀⠀⠀⢸⣿⣿⠀⠀⢀⣼⡀⣨⣿⡿⠁⠀
+// ⠀⠀⠀⠀⠀⠈⠻⠿⠿⠓⠄⠤⠘⠉⠙⠤⢀⠾⠿⣿⠟⠋
